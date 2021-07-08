26d4c8ee530314f70e58cef2925a5ec64a14576c
Akndju <davidd.akindoju@gmail.com>
2021-07-08 15:13:23 +0100

    Started css based app to practice css

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../NavBar/NavBar";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <NavBar />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import "./NavBar.scss";
+
+const NavBar = () => {
+  return (
+    <div className="navBar">
+      <div className="navBar__akindoju">Akindoju</div>
+      <div>
+        <ul className="navBar__items">
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
