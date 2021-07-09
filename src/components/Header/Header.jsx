76de96bbfda85d4b72b0d580fccb13a4e227f757
Akndju <davidd.akindoju@gmail.com>
2021-07-09 11:41:38 +0100

    Introduced extra images and mixins

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import "./Header.scss";
 
 const Header = () => {
+  const [imageOne, setImageOne] = useState(true);
+  const [imageTwo, setImageTwo] = useState(false);
+  const [imageThree, setImageThree] = useState(false);
+
   return (
-    <div className="header">
+    <div
+      className={
+        imageOne
+          ? "imageOne "
+          : imageTwo
+          ? " imageTwo "
+          : imageThree
+          ? "  imageThree "
+          : "header"
+      }
+    >
       <NavBar />
+      <div className="header-text-container">
+        <div className="header__text">
+          <h1 className="header__text--main">
+            Shoot for <br /> the hoop!
+          </h1>
+          <h4 className="header__text--sub">Aiming low is not an option.</h4>
+        </div>
+      </div>
     </div>
   );
 };
