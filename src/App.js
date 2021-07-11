517d01eca97c38c64dde8c5b0195f75b28c0bdaf
Akndju <davidd.akindoju@gmail.com>
2021-07-11 18:07:44 +0100

    Created latestCreations section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Brief from "./components/Brief/Brief";
 import Header from "./components/Header/Header";
+import LatestCreations from "./components/LatestCreations/LatestCreations";
 import Reviews from "./components/Reviews/Reviews";
 import Stats from "./components/Stats/Stats";
 import ThingsToDo from "./components/ThingsToDo/ThingsToDo";
@@ -13,6 +14,7 @@ function App() {
       <Stats />
       <ThingsToDo />
       <Reviews />
+      <LatestCreations />
     </div>
   );
 }
---
+++ src/components/LatestCreations/LatestCreations.jsx
@@ -0,0 +1,75 @@
+import "./LatestCreations.scss";
+
+const LatestCreations = () => {
+  return (
+    <div className="latestCreationsContainer">
+      <div className="latestCreationsContainer__write-up">
+        <h1>Latest Creations</h1>
+        <h4>Make Beautiful things</h4>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates
+          voluptate delectus repellat laborum corrupti commodi provident nam
+          libero similique. Doloribus!
+        </p>
+      </div>
+
+      <figure className="latestCreations--1">
+        <img
+          className="latestCreations__img"
+          src="https://images.pexels.com/photos/6193967/pexels-photo-6193967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="img 1"
+        />
+      </figure>
+
+      <figure className="latestCreations--2">
+        <img
+          className="latestCreations__img"
+          src="https://images.pexels.com/photos/7792864/pexels-photo-7792864.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="img 2"
+        />
+      </figure>
+
+      <figure className="latestCreations--3">
+        <img
+          className="latestCreations__img"
+          src="https://images.pexels.com/photos/670720/pexels-photo-670720.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="img 3"
+        />
+      </figure>
+
+      <figure className="latestCreations--4">
+        <img
+          className="latestCreations__img"
+          src="https://images.pexels.com/photos/5828538/pexels-photo-5828538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="img 4"
+        />
+      </figure>
+
+      <figure className="latestCreations--5">
+        <img
+          className="latestCreations__img"
+          src="https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="img 5"
+        />
+      </figure>
+
+      <figure className="latestCreations--6">
+        <img
+          className="latestCreations__img"
+          src="https://daks2k3a4ib2z.cloudfront.net/5527a015715879bc5767ece2/552b910bede1286e5f007d0c_port5.jpg"
+          alt="img 6"
+        />
+      </figure>
+
+      <figure className="latestCreations--7">
+        <img
+          className="latestCreations__img"
+          src="https://images.pexels.com/photos/136412/pexels-photo-136412.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt="img 7"
+        />
+      </figure>
+    </div>
+  );
+};
+
+export default LatestCreations;
