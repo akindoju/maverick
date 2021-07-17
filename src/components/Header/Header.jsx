608ef7d1168593aea5449ffc65f17df95f7310fb
Akndju <davidd.akindoju@gmail.com>
2021-07-17 16:58:53 +0100

    Added media queries for 1000px maxwidth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,39 +4,6 @@ import WOW from "wowjs";
 import "./Header.scss";
 
 const Header = () => {
-  // const [imageOne, setImageOne] = useState(true);
-  // const [imageTwo, setImageTwo] = useState(false);
-  // const [imageThree, setImageThree] = useState(false);
-
-  //function to create slideshow
-  // useEffect(() => {
-  //   const setImage = () => {
-  //     setTimeout(() => {
-  //       setImageOne(true);
-  //       setImageTwo(false);
-  //       setImageThree(false);
-  //     }, 0);
-
-  //     setTimeout(() => {
-  //       setImageOne(false);
-  //       setImageTwo(true);
-  //       setImageThree(false);
-  //     }, 7000);
-
-  //     setTimeout(() => {
-  //       setImageOne(false);
-  //       setImageTwo(false);
-  //       setImageThree(true);
-  //     }, 14000);
-  //   };
-
-  //   setImage();
-
-  //   setInterval(() => {
-  //     setImage();
-  //   }, 21000);
-  // }, []);
-
   const sliderImages = [
     {
       image:
@@ -80,7 +47,7 @@ const Header = () => {
   //creating slideshow
   setTimeout(() => {
     setCurrentCount(currentCount === length - 1 ? 0 : currentCount + 1);
-  }, 7000);
+  }, 700000);
 
   return (
     <div className="header">
@@ -118,33 +85,3 @@ const Header = () => {
 };
 
 export default Header;
-
-/* <div className="header-text-container">
-        {imageOne ? (
-          <div className="header__text wow fadeInRight">
-            <h1 className="header__text--main">
-              Shoot for <br /> the hoop!
-            </h1>
-            <h4 className="header__text--sub">Aiming low is not an option.</h4>
-          </div>
-        ) : imageTwo ? (
-          <div className="header__text">
-            <h1 className="header__text--main">
-              The meaning <br /> of life?
-            </h1>
-            <h4 className="header__text--sub">to give life meaning.</h4>
-          </div>
-        ) : imageThree ? (
-          <div className="header__text ">
-            <h1 className="header__text--main">
-              Love for all. <br /> Hatred for none!
-            </h1>
-          </div>
-        ) : (
-          <div className="header__text wow fadeInRight">
-            <h1 className="header__text--main">
-              If the world was blind <br /> how many people would you impress?
-            </h1>
-          </div>
-        )}
-      </div> */
